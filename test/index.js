ba03f2f1c87f87c985a347e948c96c9d0982abf6
Robert Grimm <3990077+apparebit@users.noreply.github.com>
2019-12-06 01:38:57 -0500

    Clearly and succinctly communicate the result of testing: Yay or nay?!

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@
 
 import { rmdir, toDirectory } from '../lib/tooling/fs.js';
 import { join } from 'path';
+import tap from 'tap';
 
 const ROOT = join(toDirectory(import.meta.url), '..');
 const COVERAGE_DATA = join(ROOT, '.coverage');
@@ -9,6 +10,26 @@ const COVERAGE_DATA = join(ROOT, '.coverage');
 
 (async function run() {
   await rmdir(COVERAGE_DATA, { recursive: true });
-  await import('./reloader.spec.js');
-  await import('./tooling.spec.js');
+  await import('./reloader.test.js');
+  await import('./tooling.test.js');
+  await import('./markup.test.js');
+
+  tap.on('end', () => {
+    let color, slogan;
+    if (tap.passing()) {
+      color = '102;1';
+      slogan = `  Yay, all ${tap.counts.total} tests passed!  `;
+    } else {
+      color = '48;5;210;1';
+      slogan = `  Nay, ${tap.counts.fail} out of ${tap.counts.total} tests failed!  `;
+      process.exitCode = 70; // X_SOFTWARE
+    }
+    const spacer = Array(slogan.length)
+      .fill(' ')
+      .join('');
+    for (const text of [spacer, slogan, spacer]) {
+      console.log(`\x1b[${color}m${text}\x1b[0m`);
+    }
+    console.log();
+  });
 })();

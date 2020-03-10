fe00931c87a79cadf9b749e624afde446d4ec3eb
Robert Grimm <3990077+apparebit@users.noreply.github.com>
2020-03-10 18:56:24 -0400

    Add new module to start site:forge with module loader hook

---
+++ tool/start.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { spawn } from 'child_process';
+
+const __directory = dirname(fileURLToPath(import.meta.url));
+const loader = join(__directory, 'loader.js');
+const cli = join(__directory, 'cli.js');
+const LOADER_HOOK = '@grr/siteforge/loader/hook';
+
+function runWithLoader() {
+  // A more targeted and hence much simpler version of foreground-child
+  // (https://github.com/tapjs/foreground-child/blob/master/index.js)
+  const { argv, argv0, cwd, execArgv } = process;
+
+  const child = spawn(
+    // Executable and most arguments are exactly the same as before, with the
+    // critical difference being the activation of resolve module loader hook.
+    argv0,
+    [...execArgv, '--experimental-loader', loader, cli, ...argv.slice(2)],
+    {
+      cwd: cwd(),
+      stdio: 'inherit',
+    }
+  );
+
+  const signalHangup = () => child.kill('SIGHUP');
+  process.on('exit', signalHangup);
+
+  child.on('close', (code, signal) => {
+    process.exitCode = signal ? 128 + signal : code;
+  });
+}
+
+async function start() {
+  if (global[LOADER_HOOK]) {
+    await import(cli);
+  } else {
+    runWithLoader();
+  }
+}
+
+start();

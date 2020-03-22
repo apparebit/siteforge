bd00939d720ec174f236feef950cb0d296df1a8e
Robert Grimm <3990077+apparebit@users.noreply.github.com>
2020-03-22 04:06:04 -0400

    Make executor and metrics part of top-level context; split build into phases for pages; stop returning so aggressively

--- cli/main.js
+++ cli/main.js
@@ -9,7 +9,7 @@ import { KIND } from '@grr/inventory/path';
 import { Logger, Metrics } from '@grr/operations';
 import { join, resolve } from 'path';
 import run from '@grr/run';
-import selectBuilderFor from '@grr/contentforge';
+import { prebuilderFor, builderFor } from '@grr/contentforge';
 import vnuPath from 'vnu-jar';
 import walk from '@grr/walk';
 
@@ -34,7 +34,7 @@ const LOADER_HOOK = '@grr/siteforge/loader/hook';
 // -----------------------------------------------------------------------------
 // Inventory of File System
 
-async function takeInventory(executor, config) {
+async function takeInventory(config) {
   const inventory = new Inventory({
     isStaticAsset: config.options.staticAssets,
   });
@@ -46,7 +46,7 @@ async function takeInventory(executor, config) {
       const { kind } = inventory.add(path, { source });
       config.logger.info(`Adding ${kind} "${path}" to inventory`);
     },
-    run: (...args) => executor.submit(...args),
+    run: (...args) => config.executor.submit(...args),
   }).done;
 
   return inventory;
@@ -55,24 +55,40 @@ async function takeInventory(executor, config) {
 // -----------------------------------------------------------------------------
 // Build
 
-async function build(executor, config) {
-  for (const phase of [1, 2, 3]) {
+const doBuild = (label, builder, file, config) => {
+  if (builder) {
+    config.executor.run(builder, undefined, file, config).catch(reason => {
+      config.logger.error(`Failed to ${label} "${file.path}"`, reason);
+    });
+  } else {
+    config.logger.error(`No ${label}er for ${file.kind} "${file.path}"`);
+  }
+};
+
+async function build(config) {
+  for (const phase of [1, 2]) {
     for (const file of config.inventory.byPhase(phase)) {
-      const builder = selectBuilderFor(file.kind);
-      if (builder) {
-        executor.run(builder, undefined, file, config).catch(reason => {
-          config.logger.error(`Failed to build "${file.path}"`, reason);
-        });
-      } else {
-        config.logger.error(`No builder for ${file.kind} "${file.path}"`);
-      }
+      doBuild('build', builderFor(file.kind), file, config);
     }
 
     // We effectively implement a poor man's version of structured concurrency:
     // All tasks spawned in an iteration of the outer loop are joined again by
     // the following await.
-    await executor.onIdle();
+    await config.executor.onIdle();
   }
+
+  const content = config.inventory.byPhase(3);
+  // (1) Determine page metadata for global indexing.
+  for (const file of content) {
+    doBuild('pre-build', prebuilderFor(file.kind), file, config);
+  }
+  await config.executor.onIdle();
+
+  // (2) Actually generate pages.
+  for (const file of content) {
+    doBuild('build', builderFor(file.kind), file, config);
+  }
+  await config.executor.onIdle();
 }
 
 // -----------------------------------------------------------------------------
@@ -150,10 +166,12 @@ async function main() {
   try {
     config = await configure();
 
+    config.executor = new Executor();
     config.logger = new Logger({
-      inJSON: config.options.logJson,
+      json: config.options.json,
       volume: config.options.volume,
     });
+    config.metrics = metrics;
   } catch (x) {
     config = { options: { help: true }, logger: new Logger() };
     config.logger.error(x.message);
@@ -202,8 +220,6 @@ async function main() {
   // ---------------------------------------------------------------------------
   // Build File System Inventory
 
-  const executor = new Executor();
-
   if (
     config.options.htaccess ||
     config.options.build ||
@@ -211,7 +227,7 @@ async function main() {
   ) {
     task(config, `Create inventory of "${config.options.contentDir}"`);
     try {
-      config.inventory = await takeInventory(executor, config);
+      config.inventory = await takeInventory(config);
     } catch (x) {
       config.logger.error(`Unable to read file system hierarchy:`, x);
       process.exitCode = 74; // EX_IOERR
@@ -236,13 +252,15 @@ async function main() {
 
   if (config.options.build) {
     task(config, `Generate build in "${config.options.buildDir}"`);
-    await build(executor, config);
+    await build(config);
   }
 
   // ---------------------------------------------------------------------------
   // Validate Markup
 
-  if (config.options.validate) {
+  if (config.options.validate && config.logger.errors) {
+    config.logger.warning(`Build has errors, skipping validation`);
+  } else if (config.options.validate) {
     task(config, `Validate markup in "${config.options.buildDir}"`);
 
     try {
@@ -250,20 +268,20 @@ async function main() {
     } catch (x) {
       config.logger.error(`Markup did not validate`, x);
       process.exitCode = 65; // EX_DATAERR
-      return;
     }
   }
 
   // ---------------------------------------------------------------------------
   // Deploy Generated Website
 
-  if (config.options.deploy) {
+  if (config.options.deploy && config.logger.errors) {
+    config.logger.warning(`Build has errors, skipping deployment`);
+  } else if (config.options.deploy) {
     if (!config.options.deploymentDir) {
       config.logger.error(
         `option "deployment-dir" must be defined to deploy website`
       );
       process.exitCode = 78; // EX_CONFIG
-      return;
     }
 
     task(config, `Deploy to "${config.options.deploymentDir}"`);

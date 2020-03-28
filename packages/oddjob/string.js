f39c6271d0fc31948f29cec258a3709ae40a2938
Robert Grimm <3990077+apparebit@users.noreply.github.com>
2020-03-28 03:51:41 -0400

    Add more overrides to make result more readable

--- packages/oddjob/string.js
+++ packages/oddjob/string.js
@@ -1,38 +1,53 @@
-/* © 2019-2020 Robert Grimm */
+/* © 2020 Robert Grimm */
 
 const { create } = Object;
 
+// Overrides to be applied before NFKD normalization written as equivalence
+// classes with source characters following their replacement.
 const OVERRIDES = [
   ['Aa', 'Å'],
   ['aa', 'å'],
   ['Ae', 'Ä', 'Æ', 'Ǽ', 'Ǣ'],
   ['ae', 'ä', 'æ', 'ǽ', 'ǣ'],
+  ['(C)', '©'],
   ['D', 'Ð'],
   ['d', 'ð'],
   ['H', 'Ħ'],
   ['h', 'ħ'],
+  ['Hv', 'Ƕ'],
+  ['hv', 'ƕ'],
   ['L', 'Ł'],
   ['l', 'ł'],
   ['Oe', 'Ø', 'Ö', 'Œ'],
   ['oe', 'ø', 'ö', 'œ'],
   ['pH', '㏗'],
+  ['(R)', '®'],
   ['SS', 'ẞ'], // Two capitalized 'S' characters indeed!
   ['ss', 'ß'],
   ['Ue', 'Ü'],
   ['ue', 'ü'],
+  ['w', 'Ƿ'],
+  ['+/-', '±'],
+  ['<<', '«'],
+  ['>>', '»'],
+  ['*', '×'],
+  ['/', '÷'],
 ];
 
+// Corrections to be applied after NFKD normalization.
 const CORRECTIONS = [
   [`'`, '\u02BC', '\u02BE'],
   ['/', '\u2215'],
   ['-', '\u2010', '\u2013', '\u2014'],
 ];
 
+// Convert equivalence classes into a predicate matching original characters.
 const toPredicate = equivalences => {
   const chars = equivalences.flatMap(alt => alt.slice(1)).join('');
   return new RegExp(`[${chars}]`, `gu`);
 };
 
+// Convert equivalence classes into object mapping originals to replacements.
 const toTable = equivalences => {
   const table = create(null);
   for (const [value, ...keys] of equivalences) {
@@ -51,6 +66,7 @@ const IS_DIACRITIC = /[\u0300-\u036f]/gu;
 const IS_DASHING_SPACING = /[\s-]+/gu;
 const IS_NOT_SLUG_SAFE = /[^-a-z0-9_]/gu;
 
+/** Convert the given extended Latin text to its ASCII equivalent. */
 export function asciify(text) {
   return text
     .replace(IS_OVERRIDE, c => GET_OVERRIDE[c])
@@ -59,6 +75,7 @@ export function asciify(text) {
     .replace(IS_CORRECTION, c => GET_CORRECTION[c]);
 }
 
+/** Convert the given extended Latin text to a slug. */
 export function slugify(text) {
   return asciify(text)
     .toLowerCase()

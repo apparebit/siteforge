0d5a8da169af8a9c284141ae06544cfe66659939
Robert Grimm <3990077+apparebit@users.noreply.github.com>
2020-04-02 22:45:23 -0400

    Add traceErrorPosition() helper and rewrite factory for TracelessError
    It now correctly identifies itself as an error without a trace

--- packages/oddjob/error.js
+++ packages/oddjob/error.js
@@ -3,20 +3,46 @@
 import { types } from 'util';
 
 const BuiltinError = Error;
+const configurable = true;
+const { defineProperty } = Object;
 const { isNativeError } = types;
 
 /** Determine whether the value is an error object. */
 export function isError(value) {
-  return isNativeError(value) || value instanceof Error;
+  return isNativeError(value) || value instanceof BuiltinError;
 }
 
+class TracelessErrorType extends BuiltinError {
+  get name() {
+    return 'TracelessError';
+  }
+}
+
+defineProperty(TracelessErrorType, 'name', {
+  configurable,
+  value: 'TracelessError',
+});
+
 /** Create a new error with the given message and no stack trace. */
-export function TracelessError(message, Error = BuiltinError) {
+export function TracelessError(message) {
   const { stackTraceLimit } = BuiltinError;
   BuiltinError.stackTraceLimit = 0;
   try {
-    return new Error(message);
+    return new TracelessErrorType(message);
   } finally {
     BuiltinError.stackTraceLimit = stackTraceLimit;
   }
+}
+
+/** Extract the given error's position trace as an array. */
+export function traceErrorPosition(error) {
+  const messageLineCount = (error.message.match(/\r?\n/gu) || []).length + 1;
+  return error.stack
+    .split(/\r?\n/gu)
+    .slice(messageLineCount)
+    .map(line => {
+      line = line.trimStart();
+      if (line.startsWith('at ')) line = line.slice(3);
+      return line;
+    });
 }

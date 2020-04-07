a9eb8828f7a45e1e402f84b0c28bb842b8d3b6c9
Robert Grimm <3990077+apparebit@users.noreply.github.com>
2020-04-07 03:47:54 -0400

    Add forwarding of basic signals to tool starter; also improve documentation

--- cli/start.js
+++ cli/start.js
@@ -8,11 +8,15 @@ const __directory = dirname(fileURLToPath(import.meta.url));
 const loader = join(__directory, 'loader.js');
 const main = join(__directory, 'main.js');
 const LOADER_HOOK = '@grr/siteforge/loader/hook';
+const SIGNALS = ['SIGABRT', 'SIGALRM', 'SIGHUP', 'SIGINT', 'SIGTERM'];
 
 function runWithLoader() {
-  // Node doesn't support installing a custom module loader at runtime. So we
-  // have to spawn another node process, with this process forwarding I/O and
-  // signals like foreground-child (https://github.com/tapjs/foreground-child/).
+  // Node doesn't support dynamic installation of a module loader. Instead, we
+  // spawn another process, with this process forwarding I/O and signals similar
+  // to [foreground-child](https://github.com/tapjs/foreground-child/). However,
+  // in an attempt to eliminate cruft that may have accumulated in that package
+  // over the years, we only forward signals shared between Windows, macOS, and
+  // Linux according to [signal-exit]](https://github.com/tapjs/signal-exit).
   const { argv, argv0, cwd, execArgv } = process;
 
   const child = spawn(
@@ -32,6 +36,11 @@ function runWithLoader() {
 
   const signalHangup = () => child.kill('SIGHUP');
   process.on('exit', signalHangup);
+
+  for (const signal of SIGNALS) {
+    const handler = () => child.kill(signal);
+    process.on(signal, handler);
+  }
 
   child.on('close', (code, signal) => {
     process.exitCode = signal ? 128 + signal : code;

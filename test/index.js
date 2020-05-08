87a2d3022bd81eab73b68c5da0364509abf35041
Robert Grimm <3990077+apparebit@users.noreply.github.com>
2020-05-08 19:22:38 -0400

    Change test script to suppress unnecessary TAP output
    This leverages new functionality in @grr/ rollcall and also changes functionality around.

--- test/index.js
+++ test/index.js
@@ -4,15 +4,40 @@ import { rmdir, toDirectory } from '@grr/fs';
 import harness from './harness.js';
 import { join } from 'path';
 import Metrics from '@grr/metrics';
+import Parser from 'tap-parser';
+import { pipeline as doPipeline } from 'stream';
+import { promisify } from 'util';
 import Rollcall from '@grr/rollcall';
 
 const ROOT = join(toDirectory(import.meta.url), '..');
 const COVERAGE_DATA = join(ROOT, '.coverage');
 
-(async function run() {
-  const metrics = new Metrics();
-  const endMain = metrics.timer('main').start();
+// Tap is a readable stream and tap-parser is a writable stream. That means
+// reasonable error reporting is only a few event handlers and a `pipeline`
+// away.
 
+const metrics = new Metrics();
+const endTest = metrics.timer('main').start();
+
+const parser = new Parser({});
+const rollcall = new Rollcall({});
+parser.on('result', r => rollcall.report(r));
+
+const done = () => {
+  const { pass, fail } = harness.counts;
+  rollcall.signOff({
+    pass,
+    fail,
+    duration: endTest().get(),
+  });
+};
+
+process.on('exit', done);
+
+const pipeline = promisify(doPipeline);
+pipeline(harness, parser);
+
+(async function run() {
   await rmdir(COVERAGE_DATA, { recursive: true });
   await import('./async.js');
   await import('./builder.js');
@@ -29,19 +54,4 @@ const COVERAGE_DATA = join(ROOT, '.coverage');
   await import('./sequitur.js');
   await import('./walk.js');
   await import('./run.js');
-
-  const done = () => {
-    const { pass, fail } = harness.counts;
-    new Rollcall({ banner: true }).signOff({
-      pass,
-      fail,
-      duration: endMain().get(),
-    });
-  };
-
-  if (harness.onFinish) {
-    harness.onFinish(done);
-  } else {
-    harness.on('end', done);
-  }
 })();

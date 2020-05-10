9b3e66ba9e3d569d5afe9aa67e45289c4a21020c
Robert Grimm <3990077+apparebit@users.noreply.github.com>
2020-05-10 13:44:45 -0400

    Add more tests for @grr/rollcall
    We still need evermore but this is progress.

--- test/rollcall.js
+++ test/rollcall.js
@@ -12,7 +12,7 @@ harness.test('@grr/rollcall', t => {
   const println = line => logged.push(line);
 
   // ------------------------------------------------------------ Logging JSON
-  let logger = new Rollcall({ println, json: true });
+  let logger = new Rollcall({ println, json: true, service: 'SITE:FORGE' });
 
   logger.notice(`The message`, ['More details']);
   let record = parseJSON(logged.shift());
@@ -22,6 +22,7 @@ harness.test('@grr/rollcall', t => {
   t.is(record.message, 'The message');
   t.same(record.data, ['More details']);
 
+  logger = new Rollcall({ println, json: true });
   logger.warning(`Keep your distance!`);
   record = parseJSON(logged.shift());
   t.ok(has(record, 'timestamp'));
@@ -51,17 +52,71 @@ harness.test('@grr/rollcall', t => {
     volume: 2,
   });
 
-  logger.debug(`Testing logger`);
-  record = logged.shift();
-  t.is(record, `[DEBUG] site:forge: Testing logger`);
+  logger.debug(`testing message by itself`);
+  t.is(logged.shift(), `[site:forge] [DEBUG] testing message by itself`);
+
+  logger.notice('testing numeric detail', 665);
+  t.is(logged.shift(), '[site:forge] [NOTE]  testing numeric detail');
+
+  logger.notice('testing object property detail', { key: 'value' });
+  t.is(logged.shift(), '[site:forge] [NOTE]  testing object property detail');
+  t.is(logged.shift(), 'key: value');
+
+  logger.notice('testing array element detail', [665]);
+  t.is(logged.shift(), '[site:forge] [NOTE]  testing array element detail');
+  t.is(logged.shift(), '665');
+
+  let err = new Error('boo!');
+  err.stack = '';
+  logger.error('testing error detail', err);
+  t.is(logged.shift(), '[site:forge] [ERROR] testing error detail: boo!');
+
+  err.stack = 'boo!\n    at Type.method (file:0:0)';
+  logger.error('testing error detail', err);
+  t.is(logged.shift(), '[site:forge] [ERROR] testing error detail');
+  t.is(logged.shift(), 'boo!');
+  t.is(logged.shift(), '    at Type.method (file:0:0)');
+
+  // signOff() is about to set the exitCode, so save value and restore later.
+  const { exitCode } = process;
 
   logger.signOff({ files: 42, duration: 665 });
-  record = logged.shift();
+  const didIt = `Processed 42 files in 665 ms`;
+  t.is(logged.shift(), `[site:forge] [ERROR] ${didIt} with 2 errors!`);
+  t.is(process.exitCode, 70);
+
+  logger.warnings = 3;
+  logger.signOff({ files: 42, duration: 665 });
   t.is(
-    record,
-    `[INFO]  ` +
-      `site:forge processed 42 files in 665 ms with no errors and no warnings!`
+    logged.shift(),
+    `[site:forge] [ERROR] ${didIt} with 2 errors and 3 warnings!`
   );
 
+  logger.errors = 0;
+  logger.signOff({ files: 42, duration: 665 });
+  t.is(logged.shift(), `[site:forge] [WARN]  ${didIt} with 3 warnings!`);
+
+  logger.warnings = 0;
+  logger.signOff({ files: 42, duration: 665 });
+  t.is(
+    logged.shift(),
+    `[site:forge] [NOTE]  ${didIt} with no errors and no warnings!`
+  );
+
+  logger.signOff({ pass: 33, fail: 9, duration: 665 });
+  t.is(
+    logged.shift(),
+    `[site:forge] [ERROR] 9 out of 42 tests failed in 665 ms!`
+  );
+
+  logger.signOff({ duration: 665 });
+  t.is(
+    logged.shift(),
+    `[site:forge] [NOTE]  Ran for 665 ms with no errors and no warnings!`
+  );
+
+  t.is(logger.embolden('That is <b>bold</b>!'), 'That is bold!');
+
+  process.exitCode = exitCode;
   t.end();
 });

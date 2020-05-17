c881a9e92755943a826068793971cdda826fe507
Robert Grimm <3990077+apparebit@users.noreply.github.com>
2020-05-17 03:56:54 -0400

    Update Model schema to latest version of @grr/schemata
    It now is functionally complete save namespace checks.

--- packages/html/schema.js
+++ packages/html/schema.js
@@ -1,12 +1,15 @@
 import {
+  All,
   Any,
   Array,
-  AtLeastOne,
   Check,
-  Distinct,
-  OneOf,
+  Enum,
+  From,
+  IntoMap,
+  IntoSet,
   Properties,
   String,
+  WithAtLeastOne,
 } from '@grr/schemata';
 
 import Context from '@grr/schemata/context';
@@ -67,15 +70,18 @@ const normalizeEnum = (value, state, context) => {
   }
 };
 
-const Attribute = Properties(
-  {
-    type: OneOf(ATTRIBUTE_TYPE),
-    enum: Array(Any(String, Properties({ default: String })), {
-      distinct: true,
-      normalize: normalizeEnum,
-    }),
-  },
-  AtLeastOne
+const Attribute = All(
+  Properties(
+    {
+      type: Enum(ATTRIBUTE_TYPE),
+      enum: Array(Any(String, Properties({ default: String })), {
+        distinct: true,
+        normalize: normalizeEnum,
+      }),
+    },
+    WithAtLeastOne
+  ),
+  Properties({ separator: Enum('comma', 'space') })
 );
 
 const AttributeCases = Properties({
@@ -87,69 +93,96 @@ const AttributeCases = Properties({
   ),
 });
 
+const WithoutCommentAndWildcard = {
+  filter: ([key]) => key !== '//' && key !== '*',
+};
+
 const Attributes = Properties({
-  attributes: Check(
-    `should describe HTML attribute via "cases", "type", or "enum" properties`,
-    Any(AttributeCases, Attribute)
+  attributes: IntoMap(
+    Properties(
+      Check(
+        `should describe HTML attribute via "cases", "type", or "enum" properties`,
+        Any(AttributeCases, Attribute)
+      ),
+      WithoutCommentAndWildcard
+    )
   ),
 });
 
+const GlobalAttributes = From(
+  ['elements', '*'],
+  IntoSet(
+    Check('should be an array listing the global attributes', Array(String))
+  )
+);
+
 const Categories = Properties({
-  categories: Check(
-    'should list HTML element names belonging to category',
-    Array(String, Distinct)
+  categories: IntoMap(
+    Properties(
+      IntoSet(
+        Check(
+          'should list HTML element names belonging to category',
+          Array(String)
+        )
+      ),
+      WithoutCommentAndWildcard
+    )
   ),
 });
 
 const ElementContent = Properties(
   {
-    category: OneOf(CONTENT_CATEGORY),
+    category: Enum(CONTENT_CATEGORY),
     elements: Check(
       `should list HTML elements valid as content`,
       Array(String)
     ),
   },
-  AtLeastOne
+  WithAtLeastOne
+);
+
+const Element = Check(
+  `should describe HTML element via "attributes" and "content" properties`,
+  Properties(
+    {
+      attributes: Check(
+        `should list HTML element's attribute names`,
+        Array(String)
+      ),
+      content: Check(
+        `should describe HTML element's content via "category" and "elements"`,
+        ElementContent
+      ),
+    },
+    WithAtLeastOne
+  )
 );
 
 const Elements = Properties({
-  elements: Check(
-    `should describe HTML element via "attributes" and "content" properties`,
-    Properties(
-      {
-        attributes: Check(
-          `should list HTML element's attribute names`,
-          Array(String, Distinct)
-        ),
-        content: Check(
-          `should describe HTML element's content via "category" and "elements"`,
-          ElementContent
-        ),
-      },
-      AtLeastOne
-    )
-  ),
+  elements: IntoMap(Properties(Element, WithoutCommentAndWildcard)),
 });
 
-const EventNames = Check(
-  'should be array listing all event names',
-  Array(String, Distinct)
+const EventNames = IntoSet(
+  Check('should be array listing all event names', Array(String))
 );
 
-const Events = Properties({
-  '*': EventNames,
-  window: EventNames,
-});
-
-const Model = Context.ify(
+const Events = From(
+  'events',
   Properties({
-    attributes: Attributes,
-    categories: Categories,
-    elements: Elements,
-    events: Events,
+    events: { from: '*', schema: EventNames },
+    windowEvents: { from: 'window', schema: EventNames },
   })
 );
 
-export default function validate(data) {
-  return Model(data);
-}
+const Model = Context.ify((value, context) => {
+  context.result = {
+    ...context.resulting(Attributes),
+    globalAttributes: context.resulting(GlobalAttributes),
+    ...context.resulting(Categories),
+    ...context.resulting(Elements),
+    ...context.resulting(Events),
+  };
+  return context.hasDefects();
+});
+
+export default Model;

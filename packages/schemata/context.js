384994500ecfb9bd24bcac3fa4df1e23633db782
Robert Grimm <3990077+apparebit@users.noreply.github.com>
2020-05-17 03:56:04 -0400

    Reimplement Context and Schemata from scratch
    This iteration fixes the handling of semantically equivalent values: #value now only stores the original value being validated and subsequent schema functions all receive this entity as first argument. #result now stores the possibly divergent yet semantically equivalent result. It is automatically constructed by withProperties().
    
    The Schemata have been cleaned up too, superfluous schema functions and combinators were culled, some were renamed, notably OneOf() became Enum().

--- packages/schemata/context.js
+++ packages/schemata/context.js
@@ -4,17 +4,33 @@ import { strict as assert } from 'assert';
 import { EOL } from 'os';
 import { toKeyPathPath } from '@grr/oddjob/string';
 
+const EnumConstantType = new Set(['bigint', 'boolean', 'number', 'string']);
 const { isArray } = Array;
+const { isSafeInteger } = Number;
 const { iterator } = Symbol;
-const { keys: keysOf } = Object;
-
-// =============================================================================
-
 const pool = [];
 
 export default class Context {
   static assertString(value) {
     assert(typeof value === 'string');
+    return value;
+  }
+
+  static assertKey(value) {
+    assert(typeof value === 'string' || (isSafeInteger(value) && value > 0));
+    return value;
+  }
+
+  static assertKeyPath(value) {
+    assert(isArray(value));
+    for (const element of value) {
+      Context.assertKey(element);
+    }
+    return value;
+  }
+
+  static assertEnumConstant(value) {
+    assert(EnumConstantType.has(typeof value));
     return value;
   }
 
@@ -26,19 +42,28 @@ export default class Context {
   static assertFunctionArray(value) {
     assert(isArray(value));
     for (const element of value) {
-      Context.assertFunction(element);
+      assert(typeof element === 'function');
     }
     return value;
   }
 
-  static assertFunctionObject(value) {
-    assert(value != null);
+  static isObjectLike(value) {
+    if (value == null) return false;
     const type = typeof value;
-    assert(type === 'object' || type === 'function');
-    for (const key of keysOf(value)) {
-      Context.assertFunction(value[key]);
-    }
+    return type === 'object' || type === 'function';
+  }
+
+  static assertObjectLike(value) {
+    assert(Context.isObjectLike(value));
     return value;
+  }
+
+  static toIterable(generatorFn) {
+    return {
+      [iterator]() {
+        return generatorFn();
+      },
+    };
   }
 
   static assertIterable(value) {
@@ -48,18 +73,227 @@ export default class Context {
 
   // ===========================================================================
 
-  #path = [];
-  #value;
-  #savedValue;
-  #defects = [];
-  #savedDefects = 0;
+  #path = []; // The key path to the current value, empty for root.
+  #value; // The current value.
+  #result; // The current result from a nested schema invocation.
+  #defects = []; // The defects found so far.
+  #defectCount = 0; // The last checkpoint for defects.
 
-  static ify(checker) {
-    Context.assertFunction(checker);
+  /** Get the complete key path. */
+  get path() {
+    return toKeyPathPath(this.#path);
+  }
 
+  /** Get the current key. */
+  get key() {
+    const path = this.#path;
+    return path[path.length - 1];
+  }
+
+  /** Get the current value. */
+  get value() {
+    return this.#value;
+  }
+
+  /** Get the current result. */
+  get result() {
+    return this.#result;
+  }
+
+  /** Set the current result. */
+  set result(value) {
+    this.#result = value;
+  }
+
+  /**
+   * Invoke the given schema on the current value and context but, instead of
+   * returning a boolean indicating validation success or failure, return the
+   * context's result.
+   */
+  resulting(schema) {
+    schema(this.#value, this);
+    return this.#result;
+  }
+
+  // ---------------------------------------------------------------------------
+
+  /** Determine whether any defects have been found so far. */
+  hasDefects() {
+    return Boolean(this.#defects.length);
+  }
+
+  /**
+   * Record a defect. The description should be a sentence fragment that omits
+   * the subject yet also asserts that some aspect of that subject is lacking.
+   * For example, "should be an integer" or "is not an integer" are reasonable
+   * ways of expressing that the value is not an integer but should be.
+   */
+  defect(description) {
+    this.#defects.push(
+      (this.#path.length === 0 ? `Value ` : `Property ${this.path} `) +
+        description
+    );
+  }
+
+  /** Convert the defects into a single error object that can be thrown. */
+  toError() {
+    const defects = this.#defects;
+    const { length } = defects;
+    switch (length) {
+      case 0:
+        return undefined;
+      case 1:
+        return new Error(`Validation found one defect:${EOL}` + defects[0]);
+      default:
+        return new Error(
+          `Validation found ${length} defects:${EOL}` + defects.join(EOL)
+        );
+    }
+  }
+
+  // ---------------------------------------------------------------------------
+
+  /**
+   * Execute the given function within a checkpoint. This function saves the
+   * current key path length, value, as well as defect count before invoking the
+   * function and restores those values afterwards again. The current result is
+   * not saved by design.
+   */
+  withCheckpoint(fn) {
+    Context.assertFunction(fn);
+
+    const pathLength = this.#path.length;
+    const value = this.#value;
+    const defectCount = this.#defectCount;
+    this.#defectCount = this.#defects.length;
+    try {
+      return fn(value, this);
+    } finally {
+      this.#path.length = pathLength;
+      this.#value = value;
+      this.#defectCount = defectCount;
+    }
+  }
+
+  /**
+   * Determine if defects were found since the last dynamically enclosing
+   * checkpoint. If there is no such checkpoint, determine if defects were
+   * found at all.
+   */
+  hasDefectsSinceCheckpoint() {
+    return this.#defects.length > this.#defectCount;
+  }
+
+  /**
+   * Clear any defects since the last dynamically enclosing checkpoint. If there
+   * is no such checkpoint, clear all defects.
+   */
+  clearDefectsSinceCheckpoint() {
+    if (this.#defects.length > this.#defectCount) {
+      this.#defects.length = this.#defectCount;
+    }
+  }
+
+  // ---------------------------------------------------------------------------
+
+  /**
+   * Check a single, possibly deeply nested property. The given validation
+   * callback is executed within a checkpoint.
+   */
+  withKeyPath(keyPath, schema, { requireContainer = true } = {}) {
+    Context.assertKeyPath(keyPath);
+    Context.assertFunction(schema);
+
+    return this.withCheckpoint((value, context) => {
+      const path = context.#path;
+
+      for (const key of keyPath) {
+        if (!Context.isObjectLike(value)) {
+          if (requireContainer) {
+            this.defect(`is primitive and does not have properties`);
+            return false;
+          } else {
+            return true;
+          }
+        }
+
+        path.push(key);
+        value = value[key];
+      }
+
+      context.#value = value;
+      return schema(value, context);
+    });
+  }
+
+  /**
+   * Check all the properties of current value. This method collects the results
+   * of checking properties into the object returned by the `init` function;
+   * that object becomes the current result just before this method returns. The
+   * given iterable may yield `[key, schema]` 2-tuples or `[readFromKey,
+   * writeToKey, schema]` 3-tuples. The two element version applies the schema
+   * function to the named property's value and stores the result in a property
+   * with the same name. The three element version differs by reading from a
+   * different property than writing to. All schema functions are executed
+   * within the same checkpoint.
+   */
+  withProperties(
+    schemata,
+    {
+      requireContainer = true,
+      init = () => ({}),
+      collectErrorResults = false,
+    } = {}
+  ) {
+    Context.assertIterable(schemata);
+    Context.assertFunction(init);
+
+    return this.withCheckpoint((value, context) => {
+      if (!Context.isObjectLike(value)) {
+        if (requireContainer) {
+          this.defect(`is primitive and does not have properties`);
+          return false;
+        } else {
+          return true;
+        }
+      }
+
+      const path = context.#path;
+      path.push(null); // The empty slot for property keys.
+      const last = path.length - 1;
+
+      const result = init();
+      let flag = true;
+      for (let [readFrom, writeTo, schema] of schemata) {
+        if (schema === undefined) {
+          schema = writeTo;
+          writeTo = readFrom;
+        }
+
+        path[last] = readFrom;
+        const ok = schema(
+          (context.#result = context.#value = value[readFrom]),
+          context
+        );
+        if (ok || collectErrorResults) {
+          result[writeTo] = context.#result;
+        }
+        flag = flag && ok;
+      }
+
+      context.#result = result;
+      return flag;
+    });
+  }
+
+  // ---------------------------------------------------------------------------
+
+  /** Wrap the given function to be invoked with a context. */
+  static ify(fn) {
     return (value, context) => {
       if (context) {
-        return context.invoke(checker);
+        assert(value === context.#value);
+        return fn(value, context);
       }
 
       if (pool.length === 0) {
@@ -67,148 +301,26 @@ export default class Context {
       }
 
       context = pool.pop();
-      context.#value = value;
-      context.#savedValue = value;
+      context.#result = context.#value = value;
 
       try {
-        const ok = context.invoke(checker);
-        if (!context.hasDefect()) {
-          if (ok) return context.value;
-          context.addDefect(
-            `"${value}" has been rejected by checker "${
-              checker.name || checker
-            }"`
-          );
+        const ok = fn(value, context);
+        if (!context.hasDefects()) {
+          if (ok) {
+            // The validation truly succeeded.
+            return context.#result;
+          }
+          context.defect(`was rejected by "${fn.name}"`);
         }
+        // The validation failed.
         throw context.toError();
       } finally {
         context.#path.length = 0;
-        context.#value = undefined;
-        context.#savedValue = undefined;
+        context.#result = context.#value = undefined;
         context.#defects.length = 0;
-        context.#savedDefects = 0;
-
+        context.#defectCount = 0;
         pool.push(context);
       }
     };
-  }
-
-  get path() {
-    return toKeyPathPath(this.#path);
-  }
-
-  get key() {
-    return this.#path[this.#path.length - 1];
-  }
-
-  get value() {
-    return this.#value;
-  }
-
-  invoke(mapper) {
-    return mapper(this.#value, this);
-  }
-
-  map(mapper) {
-    return (this.#value = mapper(this.#value, this));
-  }
-
-  // ---------------------------------------------------------------------------
-
-  hasDefect() {
-    return this.#defects.length > 0;
-  }
-
-  addDefect(description) {
-    Context.assertString(description);
-
-    const { path } = this;
-    const entity = path === '$' ? 'Value being validated' : `Property ${path}`;
-    this.#defects.push(`${entity} ${description}`);
-  }
-
-  toError() {
-    const { length } = this.#defects;
-    if (length === 1) {
-      return new Error(
-        `Data validation identified one defect:${EOL}` + this.#defects[0]
-      );
-    } else if (length > 1) {
-      return new Error(
-        `Data validation identified ${length} defects:${EOL}` +
-          this.#defects.join(EOL)
-      );
-    } else {
-      return undefined;
-    }
-  }
-
-  // ===========================================================================
-
-  withProperties(keysAndCheckers) {
-    // Operating over all properties instead of only one property amortizes the
-    // overhead of saving and restoring the context's fields. Using an iterable
-    // over key, checker pairs ensures reusability for plain objects and arrays.
-    Context.assertIterable(keysAndCheckers);
-
-    const pathLength = this.#path.length;
-    const parent = this.#value;
-    const savedValue = this.#savedValue;
-    const savedDefects = this.#savedDefects;
-
-    const type = typeof parent;
-    if (parent == null || (type !== 'object' && type !== 'function')) {
-      this.addDefect(`is a primitive value and has no properties`);
-      return false;
-    }
-
-    this.#path.push(null);
-
-    try {
-      let result = true;
-      for (const [key, checker] of keysAndCheckers) {
-        this.#path[this.#path.length - 1] = key;
-        const value = (this.#value = parent[key]);
-        this.#savedValue = value;
-        this.#savedDefects = this.#defects.length;
-
-        const ok = this.invoke(checker);
-        if (ok && value !== this.#value) {
-          parent[key] = this.#value;
-        }
-        result = result && ok;
-      }
-      return result;
-    } finally {
-      this.#path.length = pathLength;
-      this.#value = parent;
-      this.#savedValue = savedValue;
-      this.#savedDefects = savedDefects;
-    }
-  }
-
-  // ---------------------------------------------------------------------------
-
-  withCheckpoint(checker) {
-    Context.assertFunction(checker);
-
-    const savedValue = this.#savedValue;
-    const savedDefects = this.#savedDefects;
-    try {
-      this.#savedValue = this.#value;
-      this.#savedDefects = this.#defects.length;
-      return this.invoke(checker);
-    } finally {
-      this.#savedValue = savedValue;
-      this.#savedDefects = savedDefects;
-    }
-  }
-
-  resetValue() {
-    this.#value = this.#savedValue;
-  }
-
-  resetDefects() {
-    this.#defects.length = this.#savedDefects;
   }
 }
--- packages/schemata/schemata.js
+++ packages/schemata/schemata.js
@@ -1,112 +1,62 @@
 /* © 2020 Robert Grimm */
 
+import { strict as assert } from 'assert';
 import Builtin from './builtin.js';
 import Context from './context.js';
+import { isSet } from '@grr/oddjob/types';
 
-const { freeze, keys: keysOf } = Builtin.Object;
+const { entries: entriesOf, keys: keysOf, values: valuesOf } = Builtin.Object;
 const { isArray } = Builtin.Array;
 const { isSafeInteger } = Builtin.Number;
 
-// --------------------------------------------------------- Validation Context
+// -----------------------------------------------------------------------------
 
-export { Context };
-
-// ---------------------------------------------------------- Simple Predicates
-
-export const Nullish = value => value == null;
-
-export const OneOf = (...values) => {
-  if (values.length === 1 && isArray(values[0])) {
-    values = values[0];
-  }
-  return value => values.includes(value);
-};
-
-export const ObjectLike = value => {
-  if (value == null) return false;
-  const type = typeof value;
-  return type === 'object' || type === 'function';
-};
-
-// According to the standard, Map.prototype.has and Set.prototype.has must check
-// for an internal slot with the map or set data, respectively. Hence we can
-// detect map and set instances within this realm.
-const MapPrototypeHas = Builtin.Map.prototype.has;
-export const Map = value => {
-  try {
-    MapPrototypeHas.call(value);
-    return true;
-  } catch {
-    return false;
-  }
-};
-
-const SetPrototypeHas = Builtin.Set.prototype.has;
-export const Set = value => {
-  try {
-    SetPrototypeHas.call(value);
-    return true;
-  } catch {
-    return false;
-  }
-};
-
-// -------------------------------------------------------- Defect Descriptions
-
-export const Check = (description, predicate) => {
-  Context.assertString(description);
-  Context.assertFunction(predicate);
-
-  return Context.ify((_, context) => {
-    if (context.invoke(predicate)) return true;
-    context.addDefect(description);
-    return false;
+export const Check = (description, schema) =>
+  Context.ify((value, context) => {
+    const ok = schema(value, context);
+    if (!ok) context.defect(description);
+    return ok;
   });
+
+export const Number = Check(
+  `should be a floating point number`,
+  v => typeof v === 'number'
+);
+export const Integer = Check(`should be an integer`, v => isSafeInteger(v));
+export const BigInt = Check(
+  `should be a big integer`,
+  v => typeof v === 'bigint'
+);
+export const String = Check(`should be a string`, v => typeof v === 'string');
+
+// -----------------------------------------------------------------------------
+
+export const Enum = (...constants) => {
+  if (constants.length === 1 && isSet(constants[0])) {
+    const set = constants[0];
+    for (const constant of set.values()) {
+      Context.assertEnumConstant(constant);
+    }
+    return value => set.has(value);
+  } else {
+    for (const constant of constants) {
+      Context.assertEnumConstant(constant);
+    }
+    return value => constants.includes(value);
+  }
 };
 
-export const Recheck = (description, predicate) => {
-  Context.assertString(description);
-  Context.assertFunction(predicate);
+// -----------------------------------------------------------------------------
 
-  return Context.ify((_, context) =>
-    context.withCheckpoint((_, context) => {
-      if (context.invoke(predicate)) return true;
-      context.resetDefects();
-      context.addDefect(description);
-      return false;
-    })
-  );
-};
+export const Any = schemata => {
+  Context.assertFunctionArray(schemata);
 
-// ---------------------------------------------------------------- Combinators
-
-export const All = (...checkers) => {
-  Context.assertFunctionArray(checkers);
-
-  return Context.ify((_, context) =>
-    context.withCheckpoint((_, context) => {
-      for (const checker of checkers) {
-        if (!context.invoke(checker)) {
-          context.resetValue();
-          return false;
-        }
-      }
-      return true;
-    })
-  );
-};
-
-export const Any = (...checkers) => {
-  Context.assertFunctionArray(checkers);
-
-  return Context.ify((_, context) =>
-    context.withCheckpoint((_, context) => {
-      for (const checker of checkers) {
-        if (context.invoke(checker)) {
-          context.resetDefects();
+  return Context.ify((value, context) =>
+    context.withCheckpoint((value, context) => {
+      for (const schema of schemata) {
+        if (schema(value, context)) {
+          context.clearDefectsSinceCheckpoint();
           return true;
-        } else {
-          context.resetValue();
         }
       }
       return false;
@@ -114,133 +64,187 @@ export const Any = (...checkers) => {
   );
 };
 
-export const Each = (...checkers) => {
-  Context.assertFunctionArray(checkers);
+export const All = schemata => {
+  Context.assertFunctionArray(schemata);
 
-  return Context.ify((_, context) => {
-    let result = true;
-    for (const checker of checkers) {
-      const ok = context.invoke(checker);
-      result = result && ok;
+  return Context.ify((value, context) => {
+    for (const schema of schemata) {
+      if (!schema(value, context)) {
+        return false;
+      }
     }
-    return result;
+    return true;
   });
 };
 
-export const Option = checker => Any(Nullish, checker);
+// -----------------------------------------------------------------------------
 
-// --------------------------------------------------------- Objects and Arrays
+export const Nullish = v => v == null;
+export const Option = schema => Any(Nullish, schema);
 
-export const Distinct = freeze({ distinct: true });
+export const From = (path, schema) => {
+  Context.assertKeyPath(path);
+  Context.assertFunction(schema);
 
-/**
- * Check that a value is an array whose elements validate with the given
- * checker. If the `distinct` option is `true`, a standard JavaScript set is
- * used to detect duplicate elements. The `toEquatable` option provides a hook
- * for using an alternative representation during duplicate detection. The
- * hook has `(any, Set<any>, Context) => any` as its signature.
- */
+  return Context.ify((_, context) => context.withKeyPath(path, schema));
+};
+
+// -----------------------------------------------------------------------------
+
 export const Array = (
-  checker,
-  { distinct = false, toEquatable = v => v } = {}
-) =>
-  Context.ify((_, context) => {
-    Context.assertFunction(checker);
-    Context.assertFunction(toEquatable);
+  schema,
+  { distinct = true, nonempty = true, toEquatable = v => v } = {}
+) => {
+  Context.assertFunction(schema);
+  Context.assertFunction(toEquatable);
 
-    if (!context.invoke(isArray)) {
-      context.addDefect('should be an array');
+  return Context.ify((value, context) => {
+    if (!isArray(value)) {
+      context.defect(`should be an array`);
+      return false;
+    } else if (nonempty && value.length === 0) {
+      context.defect(`should be a non-empty array`);
       return false;
     }
 
-    let wrapper = checker;
+    let wrapper = schema;
     if (distinct) {
-      const values = new Builtin.Set();
-      wrapper = (_, context) => {
-        // Check for validity before checking for distinctness.
-        if (!context.invoke(checker)) return false;
+      const seen = new Builtin.Set();
+      wrapper = (value, context) => {
+        if (!schema(value, context)) return false;
 
-        const { value } = context;
-        const equatable = toEquatable(value, values, context);
-
-        if (values.has(equatable)) {
-          context.addDefect('is repeated value');
+        const equatable = toEquatable(value, seen, context);
+        if (seen.has(equatable)) {
+          context.defect(`appears repeatedly in same array`);
           return false;
         } else {
-          values.add(equatable);
-          return context.invoke(checker);
+          seen.add(equatable);
+          return true;
         }
       };
     }
 
-    return context.withProperties(function* keysAndCheckers() {
-      const { length } = context.value;
-      for (let index = 0; index < length; index++) {
-        yield [index, wrapper];
-      }
-    });
-  });
-
-export const AtLeastOne = freeze({ lax: true, min: 1 });
-
-export const Properties = (checkers, { lax = false, min = 0 } = {}) => {
-  const isCheckerFunction = typeof checkers === 'function';
-  if (!isCheckerFunction) Context.assertFunctionArray(checkers);
-
-  return Context.ify((_, context) => {
-    const { value } = context;
-    if (!ObjectLike(value)) {
-      context.addDefect('should be an object or function');
-      return false;
-    }
-
-    return context.withProperties(function* keysAndCheckers() {
-      // Account for number of defect-free matches.
-      let matches = 0;
-
-      // Iterate over value's keys for function, over checkers' keys for object.
-      for (let key of keysOf(isCheckerFunction ? value : checkers)) {
-        const checker = isCheckerFunction ? checkers : checkers[key];
-        let effectiveChecker;
-
-        if (lax && value[key] == null) {
-          // In lax mode, we skip nullish properties. That means they validate,
-          // though they do not count as matches.
-          continue;
-        } else if (min > 0) {
-          // The min option specifies minimum number of properties that need to
-          // validate. It helps with making lax mode practically useful.
-          effectiveChecker = (_, context) => {
-            const result = context.invoke(checker);
-            if (result) matches++;
-            return result;
-          };
-        } else {
-          // Otherwise, we can just use the checker as is.
-          effectiveChecker = checker;
+    return context.withProperties(
+      Context.toIterable(function* schemata() {
+        for (let index = 0; index < value.length; index++) {
+          yield [index, wrapper];
         }
-
-        yield [key, effectiveChecker];
-
-        if (matches < min) {
-          context.addDefect(
-            `does not have at least ${min} matching properties.`
-          );
-        }
-      }
-    });
+      }),
+      { init: () => [] }
+    );
   });
 };
 
-// ---------------------------------------------------------------- Validations
+// -----------------------------------------------------------------------------
 
-export const Number = Check(
-  'should a floating point number',
-  v => typeof v === 'number'
-);
-export const Integer = Check('should be an integer', v => isSafeInteger(v));
-export const BigInt = Check(
-  'should be a big integer',
-  v => typeof v === 'bigint'
-);
-export const String = Check('should be a string', v => typeof v === 'string');
+export const WithAtLeastOne = { lax: true, min: 1 };
+
+export const Properties = (
+  schemata,
+  { filter = () => true, lax = true, min = 0 } = {}
+) => {
+  const isUniformMap = typeof schemata === 'function';
+  if (!isUniformMap) {
+    Context.assertObjectLike(schemata);
+    for (const key of keysOf(schemata)) {
+      const value = schemata[key];
+      const type = typeof value;
+
+      // Either a schema function...
+      if (type === 'function') continue;
+
+      // ...or an object with a key and a schema function.
+      assert(value != null && type === 'object');
+      Context.assertString(value.from);
+      Context.assertFunction(value.schema);
+    }
+  }
+
+  return Context.ify((value, context) => {
+    if (!Context.isObjectLike(value)) {
+      context.defect(`is not object-like and hence does not have properties`);
+      return false;
+    }
+
+    let matches = 0;
+    const countingSchema = schema => (value, context) => {
+      const ok = schema(value, context);
+      if (ok) matches++;
+      return ok;
+    };
+
+    const ok = context.withProperties(
+      Context.toIterable(function* keysAndCheckers() {
+        for (const key of keysOf(isUniformMap ? value : schemata)) {
+          if (!filter(key) || (lax && value[key] == null)) {
+            continue;
+          }
+
+          let from, schema;
+          if (isUniformMap) {
+            schema = schemata;
+          } else {
+            schema = schemata[key];
+            if (typeof schema !== 'function') {
+              from = schema.from;
+              schema = schema.schema;
+            }
+          }
+
+          if (min > 0) {
+            schema = countingSchema(schema);
+          }
+
+          if (from) {
+            yield [from, key, schema];
+          } else {
+            yield [key, schema];
+          }
+        }
+      })
+    );
+
+    if (matches < min) {
+      context.defect(`does not have ${min} or more matching properties`);
+      return false;
+    } else {
+      return ok;
+    }
+  });
+};
+
+// -----------------------------------------------------------------------------
+
+export const IntoSet = schema => {
+  Context.assertFunction(schema);
+
+  return Context.ify((value, context) => {
+    const ok = schema(value, context);
+    if (!ok) return false;
+
+    const { result } = context;
+    if (result && typeof result.values === 'function') {
+      context.result = new Builtin.Set(result.values());
+    } else {
+      context.result = new Builtin.Set(valuesOf(Object(result)));
+    }
+    return true;
+  });
+};
+
+export const IntoMap = schema => {
+  Context.assertFunction(schema);
+
+  return Context.ify((value, context) => {
+    const ok = schema(value, context);
+    if (!ok) return false;
+
+    const { result } = context;
+    if (result && typeof result.entries === 'function') {
+      context.result = new Builtin.Map(result.entries());
+    } else {
+      context.result = new Builtin.Map(entriesOf(Object(result)));
+    }
+    return true;
+  });
+};

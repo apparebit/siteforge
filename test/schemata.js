96df585d97ee7e31c98ade6e23fec4145f176475
Robert Grimm <3990077+apparebit@users.noreply.github.com>
2020-05-17 03:57:14 -0400

    Update tests to accommodate latest interface changes

--- test/schemata.js
+++ test/schemata.js
@@ -8,55 +8,124 @@ harness.test('@grr/schemata', t => {
   t.test('context', t => {
     t.is(Context.assertString('string'), 'string');
     t.throws(() => Context.assertString(665));
+
+    t.is(Context.assertKey(665), 665);
+    t.is(Context.assertKey('key'), 'key');
+    t.throws(() => Context.assertKey(665n));
+    t.throws(() => Context.assertKey(/boo/u));
+
+    t.same(Context.assertKeyPath([]), []);
+    t.same(Context.assertKeyPath(['k1', 'k2', 3]), ['k1', 'k2', 3]);
+    t.throws(() => Context.assertKeyPath({}));
+    t.throws(() => Context.assertKeyPath());
+    t.throws(() => Context.assertKeyPath(null));
+    t.throws(() => Context.assertKeyPath({ length: 3 }));
+
+    t.is(Context.assertEnumConstant(true), true);
+    t.is(Context.assertEnumConstant(665), 665);
+    t.is(Context.assertEnumConstant(665n), 665n);
+    t.is(Context.assertEnumConstant('text'), 'text');
+    t.throws(() => Context.assertEnumConstant());
+    t.throws(() => Context.assertEnumConstant(null));
+    t.throws(() => Context.assertEnumConstant({}));
+    t.throws(() => Context.assertEnumConstant(() => {}));
+
     t.is(Context.assertFunction(Context), Context);
     t.throws(() => Context.assertFunction(665));
-    t.same(Context.assertFunctionArray([Context]), [Context]);
+
+    const a1 = [];
+    const a2 = [Context];
+    t.is(Context.assertFunctionArray(a1), a1);
+    t.is(Context.assertFunctionArray(a2), a2);
     t.throws(() => Context.assertFunctionArray(665));
     t.throws(() => Context.assertFunctionArray([665]));
-    t.same(Context.assertFunctionObject({ value: Context }), {
-      value: Context,
-    });
-    t.throws(() => Context.assertFunctionObject(665));
-    t.throws(() => Context.assertFunctionObject({ value: 665 }));
+
+    const o1 = {};
+    const o2 = () => {};
+    t.is(Context.assertObjectLike(o1), o1);
+    t.is(Context.assertObjectLike(o2), o2);
+    t.throws(() => Context.assertObjectLike());
+    t.throws(() => Context.assertObjectLike(null));
+    t.throws(() => Context.assertObjectLike(665));
+
     t.same(Context.assertIterable([665]), [665]);
     t.throws(() => Context.assertIterable(665));
 
-    const bottom = Context.ify((value, context) => {
-      t.is(value, 665n);
+    // -------------------------------------------------------------------------
+
+    const Quantity = Context.ify((value, context) => {
+      t.is(value, context.value);
+      t.is(value, context.result);
+      t.is(value, 665);
       t.ok(context);
-      t.is(context.path, '$.value');
-      t.is(context.key, 'value');
-      t.is(context.value, 665n);
+      t.is(context.path, '$.quantity');
+      t.is(context.key, 'quantity');
+      context.result = 665n;
       return true;
     });
-    const top = Context.ify((value, context) => {
-      t.is(value, 665);
+    const ObjectWithQuantity = Context.ify((value, context) => {
+      t.is(value, context.value);
+      t.is(value, context.result);
+      t.same(value, { quantity: 665 });
       t.ok(context);
       t.is(context.path, '$');
       t.is(context.key, undefined);
-      t.is(context.value, 665);
-      context.map(v => ({ value: BigInt(v) }));
-      return context.withProperties([['value', bottom]]);
+      t.is(context.toError(), undefined);
+      t.notOk(context.hasDefects());
+
+      context.withCheckpoint((v, c) => {
+        t.is(c, context);
+        t.is(v, value);
+        t.notOk(c.hasDefectsSinceCheckpoint());
+        c.defect('is bad');
+        t.ok(c.hasDefectsSinceCheckpoint());
+        c.clearDefectsSinceCheckpoint();
+        t.notOk(c.hasDefectsSinceCheckpoint());
+      });
+
+      return context.withProperties([['quantity', Quantity]]);
     });
-    t.same(top(665), { value: 665n });
+    t.same(ObjectWithQuantity({ quantity: 665 }), { quantity: 665n });
+
+    const b2 = Context.ify((_, context) => {
+      context.defect(`isn't to my liking`);
+      return false;
+    });
+    const t2 = Context.ify((_, context) => {
+      t.notOk(Schemata.Properties({ b2 })(_, context));
+      context.defect(`is a bit screwy`);
+      return false;
+    });
+    t.throws(
+      () => t2({ b2: 665 }),
+      /Validation found 2 defects:\nProperty \$\.b2 isn't to my liking\nValue is a bit screwy/u
+    );
 
     const falsify = () => false;
     t.throws(
       () => Context.ify(falsify)(665),
-      /Value being validated "665" has been rejected by checker "falsify"/u
+      /Validation found one defect:\nValue was rejected by "falsify"/u
     );
+
     t.end();
   });
 
   t.ok(Schemata.Nullish());
   t.ok(Schemata.Nullish(null));
-  t.ok(!Schemata.Nullish(665));
+  t.notOk(Schemata.Nullish(665));
 
-  t.ok(!Schemata.OneOf(1, 2, 3)(0));
-  t.ok(Schemata.OneOf(1, 2, 3)(1));
-  t.ok(Schemata.OneOf(1, 2, 3)(2));
-  t.ok(Schemata.OneOf(1, 2, 3)(3));
-  t.ok(!Schemata.OneOf(1, 2, 3)(4));
+  t.notOk(Schemata.Enum(1, 2, 3)(0));
+  t.ok(Schemata.Enum(1, 2, 3)(1));
+  t.ok(Schemata.Enum(1, 2, 3)(2));
+  t.ok(Schemata.Enum(1, 2, 3)(3));
+  t.notOk(Schemata.Enum(1, 2, 3)(4));
+
+  const Zahlen = new Set([11, 22, 33, 44, 55, 66, 77, 88, 99]);
+  const Schnapszahl = Schemata.Enum(Zahlen);
+  t.notOk(Schnapszahl(10));
+  t.notOk(Schnapszahl(100));
+  t.ok(Schnapszahl(11));
+  t.ok(Schnapszahl(99));
 
   t.end();
 });

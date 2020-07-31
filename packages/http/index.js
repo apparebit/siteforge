dcae2ff9aebe0c3e00f75b998210b23fa41e9331
Robert Grimm <3990077+apparebit@users.noreply.github.com>
2020-07-30 23:24:31 -0400

    Replace assorted modules with single package entry point

---
+++ packages/http/index.js
@@ -0,0 +1,5 @@
+export { certifyLocalhost, readCertDates } from './certificate.js';
+export { default as MediaType } from './media-type.js';
+export { default as mediaTypeForPath } from './file-type.js';
+export { default as parseRequestPath } from './parse-path.js';
+//export { default as Http2Server } from './http2-server.js';

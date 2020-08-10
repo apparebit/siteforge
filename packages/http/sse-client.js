a8dc66a8b182fa75be37029a4f42de53a4a91f63
Robert Grimm <3990077+apparebit@users.noreply.github.com>
2020-08-09 23:57:54 -0400

    Add SSE client
    Module connects to SSE endpoint requesting to subscribe, it then parses events as they arrive, and exposes the events through an asynchronous generator.

---
+++ packages/http/sse-client.js
@@ -0,0 +1,63 @@
+/* © 2020 Robert Grimm */
+
+import { constants } from 'http2';
+import MediaType from './media-type.js';
+import readline from 'readline';
+import { settleable } from '@grr/async/promise';
+
+const { HTTP2_HEADER_CONTENT_TYPE, HTTP2_HEADER_STATUS } = constants;
+
+const Not200 = status =>
+  new Error(`Status code is "${status}" instead of expected "200"`);
+const isEventStream = type =>
+  type != null && type.type === 'text' && type.subtype === 'event-stream';
+const NotEventStream = type =>
+  new Error(
+    `Content type is "${type}" instead of expected "text/event-stream"`
+  );
+
+export default async function* events(session, path) {
+  const stream = session.request({
+    ':method': 'GET',
+    ':path': path,
+    accept: 'text/event-stream',
+  });
+
+  const { promise, resolve, reject } = settleable();
+
+  stream.on('response', headers => {
+    const status = headers[HTTP2_HEADER_STATUS];
+    const type = MediaType.of(headers[HTTP2_HEADER_CONTENT_TYPE]);
+
+    if (status !== 200) {
+      reject(Not200(status));
+    } else if (!isEventStream(type)) {
+      reject(NotEventStream(type));
+    } else {
+      stream.setEncoding('utf8');
+      resolve(readline.createInterface({ input: stream }));
+    }
+  });
+
+  try {
+    let event;
+    for await (const line of await promise) {
+      if (line === '') {
+        // Empty line completes event.
+        yield event;
+        event = undefined;
+      } else {
+        // Initialize only when needed.
+        if (event == null) event = {};
+
+        // Capture only if not a comment.
+        const [key, value] = line.split(':', 2);
+        if (key !== '') event[key] = value.trimStart();
+      }
+    }
+
+    if (event) yield event;
+  } finally {
+    stream.close();
+  }
+}

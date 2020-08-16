5a4c51f3fd9ef395255dbafaa4c85980ee517f3e
Robert Grimm <3990077+apparebit@users.noreply.github.com>
2020-08-16 17:00:03 -0400

    Eliminate @grr/* dependencies, rewrite refreshen() to bring out actual logic

--- packages/http/certificate.js
+++ packages/http/certificate.js
@@ -1,11 +1,12 @@
 /* © 2020 Robert Grimm */
 
 import { strict as assert } from 'assert';
+import { EOL } from 'os';
 import { fileURLToPath } from 'url';
 import { join } from 'path';
+import { parseDateOpenSSL } from './util.js';
 import { promises } from 'fs';
-import run from '@grr/run';
-import { settleable } from '@grr/async/promise';
+import { spawn } from 'child_process';
 
 const LOCALHOST_CFG = 'localhost.cfg';
 const LOCALHOST_CRT = 'localhost.crt';
@@ -19,7 +20,7 @@ const { mkdir, readFile } = promises;
  * Create a new self-signed certificate. The certificate is restricted for use
  * as a server certificate for `localhost` for 30 days. Both certificate and
  * private key are stored in the directory with the given path. This function
- * expects that OpenSSL is installed and on the path.
+ * expects that OpenSSL is installed and the executable is on the system's PATH.
  */
 export const certifyLocalhost = async ({ openssl = 'openssl', path }) => {
   // Make sure the directory exists.
@@ -37,13 +38,31 @@ export const certifyLocalhost = async ({ openssl = 'openssl', path }) => {
     '-out', LOCALHOST_CRT,
   ];
 
-  await run(openssl, args, { cwd: path });
+  const child = spawn(openssl, args, { cwd: path });
+
+  let out = '';
+  child.stdout.on('data', chunk => (out += chunk));
+  child.stderr.on('data', chunk => (out += chunk));
+
+  return new Promise((resolve, reject) => {
+    child.on('exit', (code, signal) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        const indicator = signal ? `signal "${signal}"` : `code "${code}"`;
+        const output = `output << EOM${EOL}${out}${EOL}EOM`;
+        reject(new Error(`OpenSSL terminated with ${indicator} and ${output}`));
+      }
+    });
+  });
 };
 
 // =============================================================================
 
 // Helper function to convert (part of a) certificate to text.
 const getCertInfo = ({ openssl = 'openssl', path, cert, info = '-text' }) => {
+  assert((path == null && cert != null) || (path != null && cert == null));
+
   // prettier-ignore
   const args = [
     'x509',
@@ -52,22 +71,27 @@ const getCertInfo = ({ openssl = 'openssl', path, cert, info = '-text' }) => {
     info,
   ];
 
-  const promise = run(openssl, args, { stdio: 'pipe' });
-  const { child } = promise;
-  const { stdin, stdout, stderr } = child;
+  const child = spawn(openssl, args);
 
-  // If we have the certificate in memory, write it to stdin.
-  if (cert) stdin.write(cert);
+  // Pipe certificate if in memory.
+  if (cert) child.stdin.write(cert);
 
-  // Capture stdout and stderr.
-  const cap = settleable();
+  // Capture stdout with printed information, drop stderr to floor.
   let out = '';
-  stdout.on('data', chunk => (out += chunk));
-  stderr.on('data', () => {});
-  child.on('close', () => cap.resolve(out));
+  child.stdout.on('data', chunk => (out += chunk));
+  child.stderr.on('data', () => {});
 
-  // Return a promise for the output.
-  return cap.promise;
+  return new Promise((resolve, reject) => {
+    child.on('exit', (code, signal) => {
+      if (code === 0) {
+        resolve(out);
+      } else if (signal != null) {
+        reject(new Error(`OpenSSL terminated with signal "${signal}"`));
+      } else {
+        reject(new Error(`OpenSSL terminated with code "${code}"`));
+      }
+    });
+  });
 };
 
 // -----------------------------------------------------------------------------
@@ -86,11 +110,11 @@ export const readCertDates = async ({ openssl = 'openssl', path, cert }) => {
 
   let [label, date] = lines[0].split('=');
   assert(label === 'notBefore');
-  const notBefore = new Date(date);
+  const notBefore = parseDateOpenSSL(date);
 
   [label, date] = lines[1].split('=');
   assert(label === 'notAfter');
-  const notAfter = new Date(date);
+  const notAfter = parseDateOpenSSL(date);
 
   // Done.
   return { notBefore, notAfter };
@@ -109,40 +133,50 @@ export const dumpCertificate = /* async */ ({
 
 /**
  * Load the TLS certificate and key from the given directory. If the certificate
- * does not exist, is not yet valid, or has already expired, or if the private
- * key does not exist, this function recreates a new certificate and key first.
- * That requires OpenSSL being installed and on path.
+ * does not exist, the certificate is not yet valid, the certificate is about to
+ * expire, or the private key does not exist, this function automatically
+ * recreates a new self-signed certificate and key. This function treats a
+ * certificate as expired `epsilon` seconds before its actual expiration.
+ * OpenSSL or its fork LibreSSL must be installed and on the system path.
  */
-export const refreshen = async ({ openssl = 'openssl', path }) => {
+export const refreshen = async ({
+  openssl = 'openssl',
+  path,
+  epsilon = 10 * 60,
+}) => {
   const certPath = join(path, LOCALHOST_CRT);
   const keyPath = join(path, LOCALHOST_KEY);
-  let cert, key;
 
-  let trials = 3;
-  while (--trials > 0) {
-    try {
-      // Read the certificate and key.
-      cert = await readFile(certPath);
-      key = await readFile(keyPath);
+  const readChecked = async () => {
+    // Read the certificate and private key.
+    const cert = await readFile(certPath);
+    const key = await readFile(keyPath);
 
-      // Read the not-before and not-after dates.
-      const { notBefore, notAfter } = await readCertDates({ openssl, cert });
+    // Extract the not-before and not-after dates.
+    const { notBefore, notAfter } = await readCertDates({ openssl, cert });
 
-      // Validate against current datetime and be done on success.
-      const now = new Date();
-      if (notBefore < now && now < notAfter) return { cert, key };
-    } catch (x) {
-      if (x.code !== 'ENOENT') throw x;
+    // Validate against current time.
+    const now = Date.now();
+    if (notBefore <= now && now <= notAfter - epsilon * 1000) {
+      return { cert, key };
     }
 
-    // If one of the above steps fails, we probably need a new certificate.
-    await certifyLocalhost({ openssl, path });
+    const lower = new Date(notBefore).toISOString();
+    const upper = new Date(notAfter).toISOString();
+    const error = new Error(`Certificate valid from ${lower} to ${upper}`);
+    error.code = 'CERT_INVALID';
+    throw error;
+  };
+
+  // Try to read and validate certificate (plus private key).
+  try {
+    return await readChecked();
+  } catch (x) {
+    if (x.code !== 'ENOENT' && x.code !== 'CERT_INVALID') throw x;
   }
 
-  // Something isn't quite working out. Fail loudly.
-  throw new Error(
-    `Unable to read TLS server certificate and key ` +
-      `despite three attempts at regeneration.\n` +
-      `Please check that OpenSSL is properly installed and on system path`
-  );
+  // Either the file didn't exist or the certificate wasn't valid. So we create
+  // a new self-signed certificate and try to read and validate again.
+  await certifyLocalhost({ openssl, path });
+  return readChecked();
 };

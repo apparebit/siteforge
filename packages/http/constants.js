48302d9ba7d11eed2e24aa704ff383f5b9d4819f
Robert Grimm <3990077+apparebit@users.noreply.github.com>
2020-08-29 23:15:13 -0400

    Change namespace object names Method and Status to MethodName and StatusCode
    Otherwise, they conflict with header constant names inside the Header namespace object.
    
    Also add StatusWithoutBody enumeration object and the non-standard Query header name.

--- packages/http/constants.js
+++ packages/http/constants.js
@@ -1,7 +1,7 @@
 /* © 2020 Robert Grimm */
 
 import { constants } from 'http2';
-const { create, entries: entriesOf, freeze } = Object;
+const { assign, create, entries: entriesOf, freeze } = Object;
 
 const STATUS_PREFIX = 'HTTP_STATUS_';
 const HEADER_PREFIX = 'HTTP2_HEADER_';
@@ -11,14 +11,14 @@ const reformat = key =>
   key.toLowerCase().replace(/(?:^|_)(.)/gu, (_, s) => s.toUpperCase());
 
 const prepare = () => {
-  const Status = create(null);
+  const StatusCode = create(null);
   const Header = create(null);
 
   // Reuse existing constants from http2 module.
   for (let [key, value] of entriesOf(constants)) {
     if (key.startsWith(STATUS_PREFIX)) {
       key = reformat(key.slice(STATUS_PREFIX.length));
-      Status[key] = value;
+      StatusCode[key] = value;
     } else if (key.startsWith(HEADER_PREFIX)) {
       key = key.slice(HEADER_PREFIX.length);
       if (key.startsWith(X_PREFIX)) {
@@ -29,28 +29,34 @@ const prepare = () => {
     }
   }
 
-  // Add missing constants.
-  Header.Body = ':body';
   Header.ReferrerPolicy = 'referrer-policy';
   Header.PermittedCrossDomainPolicies = 'x-permitted-cross-domain-policies';
   Header.PoweredBy = 'x-powered-by';
+  Header.Query = ':query'; // Useful when normalizing `:path`.
 
   // Only reuse HTTP/2 methods.
-  const Method = create(null);
-  Method.CONNECT = constants.HTTP2_METHOD_CONNECT;
-  Method.DELETE = constants.HTTP2_METHOD_DELETE;
-  Method.GET = constants.HTTP2_METHOD_GET;
-  Method.HEAD = constants.HTTP2_METHOD_HEAD;
-  Method.OPTIONS = constants.HTTP2_METHOD_OPTIONS;
-  Method.PATCH = constants.HTTP2_METHOD_PATCH;
-  Method.POST = constants.HTTP2_METHOD_POST;
-  Method.PUT = constants.HTTP2_METHOD_PUT;
+  const MethodName = create(null);
+  MethodName.CONNECT = constants.HTTP2_METHOD_CONNECT;
+  MethodName.DELETE = constants.HTTP2_METHOD_DELETE;
+  MethodName.GET = constants.HTTP2_METHOD_GET;
+  MethodName.HEAD = constants.HTTP2_METHOD_HEAD;
+  MethodName.OPTIONS = constants.HTTP2_METHOD_OPTIONS;
+  MethodName.PATCH = constants.HTTP2_METHOD_PATCH;
+  MethodName.POST = constants.HTTP2_METHOD_POST;
+  MethodName.PUT = constants.HTTP2_METHOD_PUT;
+
+  const StatusWithoutBody = assign(create(null), {
+    204: true,
+    205: true,
+    304: true,
+  });
 
   return {
     Header: freeze(Header),
-    Method: freeze(Method),
-    Status: freeze(Status),
+    MethodName: freeze(MethodName),
+    StatusCode: freeze(StatusCode),
+    StatusWithoutBody: freeze(StatusWithoutBody),
   };
 };
 
-export const { Header, Method, Status } = prepare();
+export const { Header, MethodName, StatusCode, StatusWithoutBody } = prepare();

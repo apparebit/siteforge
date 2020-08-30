052e096553242060ab1541c8a9e6624eac52c3d1
Robert Grimm <3990077+apparebit@users.noreply.github.com>
2020-08-29 23:12:42 -0400

    Make volume of default logger instance configurable via command line argument
    It makes debugging tests so much easier.

--- test/harness.js
+++ test/harness.js
@@ -7,9 +7,22 @@ if (!tap.Test.prototype.strictSame) {
   tap.Test.prototype.strictSame = tap.Test.prototype.same;
 }
 
+// Make the same global logger available to all tests. Also make the volume for
+// said logger easily configurable via command line flag.
+let volume = 0;
+for (const arg of process.argv) {
+  if (arg === '--') {
+    break;
+  } else if (arg === '--debug') {
+    volume = 2;
+  } else if (arg === '--trace') {
+    volume = 3;
+  }
+}
+
 Object.defineProperty(tap, 'rollcall', {
   configurable: true,
-  value: new Rollcall({ volume: 0 }),
+  value: new Rollcall({ volume }),
 });
 
 export default tap;

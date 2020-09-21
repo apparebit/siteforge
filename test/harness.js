5c12d65d761065a44dc182c35f887b46c44e9895
Robert Grimm <apparebit@gmail.com>
2020-09-21 03:34:18 -0400

    Skip arguments not specified by user
    Also update comments

--- test/harness.js
+++ test/harness.js
@@ -3,14 +3,14 @@
 import Rollcall from '@grr/rollcall';
 import tap from 'tap';
 
+// Polyfill `strictSame()` to be the same as `same()`.
 if (!tap.Test.prototype.strictSame) {
   tap.Test.prototype.strictSame = tap.Test.prototype.same;
 }
 
-// Make the same global logger available to all tests. Also make the volume for
-// said logger easily configurable via command line flag.
+// Instantiate global logger with easily configurable volume.
 let volume = 0;
-for (const arg of process.argv) {
+for (const arg of process.argv.slice(2)) {
   if (arg === '--') {
     break;
   } else if (arg === '--debug') {
@@ -19,10 +19,12 @@ for (const arg of process.argv) {
     volume = 3;
   }
 }
+const rollcall = new Rollcall({ volume });
 
+// Decorate test harness.
 Object.defineProperty(tap, 'rollcall', {
   configurable: true,
-  value: new Rollcall({ volume }),
+  value: rollcall,
 });
 
 export default tap;

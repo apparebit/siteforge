d0cde108904916f1add4a037fc3d1fd1aa14a229
Robert Grimm <apparebit@gmail.com>
2024-04-13 02:07:48 -0400

    replace VNU with html-validate

--- cli/main.js
+++ cli/main.js
@@ -6,6 +6,7 @@ import { buildAll, rebuildOnDemand } from '@grr/builder';
 import { configure, validate } from './config.js';
 import createContext from '@grr/builder/context';
 import { createDevServer } from '@grr/http';
+import { HtmlValidate } from "html-validate";
 import { join, resolve } from 'path';
 import { Kind } from '@grr/inventory/kind';
 import { networkInterfaces } from 'os';
@@ -13,7 +14,6 @@ import open from 'open';
 import process from 'process';
 import { readFile, rm, toDirectory } from '@grr/fs';
 import run from '@grr/run';
-import vnuPath from 'vnu-jar';
 import walk from '@grr/walk';
 
 const __directory = toDirectory(import.meta.url);
@@ -21,22 +21,6 @@ const BUILD_HTACCESS = resolve(
   __directory,
   '../../server-configs-apache/bin/build.sh'
 );
-const IGNORED_VALIDATIONS = [
-  `Attribute “media” not allowed on element “meta” at this point.`,
-  `.*Cannot invoke "org.w3c.css.values.CssValue.get(Raw)?Type\\(\\)" because "this.val1" is null.`,
-  `CSS: “backdrop-filter”: Property “backdrop-filter” doesn't exist.`,
-  `CSS: “background-image”: “0%” is not a “color” value.`,
-  `CSS: “color-adjust”: Property “color-adjust” doesn't exist.`,
-  `“font-size”`,
-  `CSS: “font-size”: The types are incompatible.`,
-  `CSS: “font-stretch”: “\\d{1,3}\\%” is not a “font-stretch” value.`,
-  `CSS: “inset”: Property “inset” doesn't exist.`,
-  `CSS: Parse Error.`,
-  `CSS: Unknown pseudo-element or pseudo-class “:is”`,
-  `File was not checked. Files must have \\.html, \\.xhtml, \\.htm, or \\.xht extensions.`,
-  `Possible misuse of “aria-label”`,
-  `The “contentinfo” role is unnecessary for element “footer”.`,
-];
 const { keys: keysOf } = Object;
 
 // -----------------------------------------------------------------------------
@@ -59,34 +43,44 @@ async function takeInventory(config) {
 // -----------------------------------------------------------------------------
 // Validation
 
-function validateMarkup(config) {
+async function validateMarkup(config) {
   const { inventory, logger, options } = config;
   const { justCopy } = options;
 
-  // Nu Validator's command line interface pretends to be useful but is not.
-  // Anything beyond selecting all files of a given type is impossible. That
-  // means traversing the file system before traversing the file system. Yay!
-  const paths = [];
+  const validator = new HtmlValidate({
+    extends: ["html-validate:recommended"],
+    rules: {
+      "attr-quotes": 0,
+      "no-redundant-role": 0,
+    },
+  });
+
   for (const file of inventory.byKind(Kind.Markup)) {
     if (justCopy(file.path)) continue;
     const path = join(options.buildDir, file.path);
-    paths.push(path);
+    const report = await validator.validateFile(path);
+    if (report.valid) continue;
+
+    const messages = report.results[0].messages.filter(m =>
+      (
+        m.ruleId !== "valid-id"
+        || m.message !== "element id must begin with a letter"
+      ) && (
+        m.ruleId !== "prefer-native-element"
+        || m.message !== "Prefer to use the native <ul> element"
+      )
+    );
+
+    for (const message of messages) {
+      let m = `${file.path}:${message.line}:${message.column}: `;
+      m += `${message.message} (${message.ruleId})`;
+      if (message.severity === 2) {
+        logger.error(m);
+      } else {
+        logger.warn(m);
+      }
+    }
   }
-
-  logger.info(
-    `Found ${paths.length} markup file${paths.length !== 1 ? 's' : ''
-    } to validate`
-  );
-
-  // prettier-ignore
-  return run('java', [
-    '-jar', vnuPath,
-    '--skip-non-html',
-    '--errors-only', // VNU ignores filter pattern on "possible misuse of aria-label".
-    '--filterpattern', `(${IGNORED_VALIDATIONS.join('|')})`,
-    ...(options.volume >= 2 ? ['--verbose'] : []),
-    ...paths,
-  ]);
 }
 
 // -----------------------------------------------------------------------------
@@ -138,6 +132,7 @@ async function main() {
 
   const config = validate(await createContext(configure));
   const { logger, options } = config;
+  let section = 1;
 
   // ---------------------------------------------------------------------------
   // Handle Display of Version and Help
@@ -161,7 +156,7 @@ async function main() {
     options.cleanRun &&
     !options.dryRun
   ) {
-    logger.section(1, `Clean previous build in "${options.buildDir}"`, config);
+    logger.section(section++, `Clean previous build in "${options.buildDir}"`, config);
     await rm(options.buildDir, { force: true, recursive: true });
   }
 
@@ -174,7 +169,7 @@ async function main() {
     options.build ||
     options.validate
   ) {
-    logger.section(2, `Create inventory of "${options.contentDir}"`, config);
+    logger.section(section++, `Create inventory of "${options.contentDir}"`, config);
     try {
       await takeInventory(config);
     } catch (x) {
@@ -188,7 +183,7 @@ async function main() {
   // Build .htaccess
 
   if (options.htaccess && !options.dryRun) {
-    logger.section(3.1, `Build ".htaccess"`, config);
+    logger.section(section++, `Build ".htaccess"`, config);
     try {
       await run('bash', [BUILD_HTACCESS], { cwd: options.contentDir });
     } catch (x) {
@@ -200,18 +195,18 @@ async function main() {
   // Build and Serve Content
 
   if (options.build || options.develop) {
-    logger.section(3.2, `Build website in "${options.buildDir}"`, config);
+    logger.section(section++, `Build website in "${options.buildDir}"`, config);
     await buildAll(config);
   }
 
   if (options.develop) {
-    logger.section(3.3, `Serve website in "${options.buildDir}"`, config);
+    logger.section(section++, `Serve website in "${options.buildDir}"`, config);
 
     const { stdout } = await run('ps', ['-o', 'pid,comm'], { stdio: 'buffer' });
     const match = / *(?<pid>\d+) site:forge/.exec(stdout)
     if (match) {
       const pid = Number(match[1]);
-      if (pid != process.pid) {
+      if (pid !== process.pid) {
         logger.note(`Killing previously launched site:forge with PID ${pid}`);
         process.kill(pid, 'SIGTERM');
       }
@@ -287,16 +282,20 @@ async function main() {
   // ---------------------------------------------------------------------------
   // Validate Markup
 
-  if (options.validate && logger.errors) {
-    logger.warning(`Build has errors, skipping validation`);
-  } else if (options.validate && !options.dryRun) {
-    logger.section(4, `Validate markup in "${options.buildDir}"`, config);
+  if (options.deploy || options.validate) {
+    if (logger.errors) {
+      logger.warn(`Build has errors, skipping validation`);
+    } else if (options.dryRun) {
+      logger.note(`Dry run, skipping validation`);
+    } else {
+      logger.section(section++, `Validate markup in "${options.buildDir}"`, config);
 
-    try {
-      await validateMarkup(config);
-    } catch (x) {
-      logger.error(`Markup did not validate`, x);
-      process.exitCode = 65; // EX_DATAERR
+      try {
+        await validateMarkup(config);
+      } catch (x) {
+        logger.error(`Markup did not validate`, x);
+        process.exitCode = 65; // EX_DATAERR
+      }
     }
   }
 
@@ -304,9 +303,9 @@ async function main() {
   // Deploy Generated Website
 
   if (options.deploy && logger.errors) {
-    logger.warning(`Build has errors, skipping deployment`);
+    logger.warn(`Build has errors, skipping deployment`);
   } else if (options.deploy) {
-    logger.section(5, `Deploy to "${options.deploymentHost}"`, config);
+    logger.section(section++, `Deploy to "${options.deploymentHost}"`, config);
     await deploy(config);
   }
 
